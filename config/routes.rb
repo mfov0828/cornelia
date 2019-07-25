d83ee5ae0ed91f307fc2108c0fe4d1be7422b1ef
Manuel Fernando Ostos Vanegas <mfov0828@gmail.com>
2019-07-24 19:48:53 -0500

    Commit Git

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  #resources :products
+  get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
+  post '/products', to: 'products#create'
+  get '/product/:id', to: 'products#show', as: 'product'
+  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
+  patch '/product/:id', to: 'products#update'
+  put '/product/:id', to: 'products#update'
+  delete '/product/:id', to: 'products#delete'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
